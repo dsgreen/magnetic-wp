34b560c9275b801e56d6a0762e6ea7a77e75821a
Doug Green <doug@dsgreen.com>
2018-09-27 13:40:47 -0400

    Add front page template

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Front page template.
+ *
+ * @package _s
+ */
+
+get_header();
+?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+<?php get_template_part( 'template-parts/image-header' ); ?>
+<section class="site-content container" id="content">
+  <div class="row">
+    <main class="col-12 col site-main" id="main" role="main">
+
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
+
+			<?php endwhile; // End of the loop. ?>
+
+    </main>
+  </div>
+</section>
+<?php get_footer();
+?>
