d065257cc735dc74ffc34e393f6e13063de3ecc5
Doug Green <doug@dsgreen.com>
2018-10-04 12:49:48 -0400

    Optional label parameters

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -78,27 +78,27 @@ if ( ! function_exists( '_s_comments_link' ) ) :
 endif;
 
 if ( ! function_exists( '_s_print_categories' ) ) :
-  function _s_print_categories() {
+  function _s_print_categories($cat_label='') {
 	  // Hide category and tag text for pages.
 	  if ( 'post' === get_post_type() ) {
 		  /* translators: used between list items, there is a space after the comma */
 		  $categories_list = get_the_category_list(esc_html__(', ', '_s'));
 		  if ($categories_list) {
 			  /* translators: 1: list of categories. */
-			  printf('<span class="cat-links">' . esc_html__('%1$s', '_s') . '</span>', $categories_list); // WPCS: XSS OK.
+			  printf('<span class="cat-links">' . esc_html__($cat_label . '%1$s', '_s') . '</span>', $categories_list); // WPCS: XSS OK.
 		  }
 	  }
   }
 endif;
 if ( ! function_exists( '_s_print_tags' ) ) :
-	function _s_print_tags() {
+	function _s_print_tags($tag_label='') {
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list('', esc_html_x(', ', 'list item separator', '_s'));
 			if ($tags_list) {
 				/* translators: 1: list of tags. */
-				printf('<span class="tags-links">' . esc_html__('%1$s', '_s') . '</span>', $tags_list); // WPCS: XSS OK.
+				printf('<span class="tags-links">' . esc_html__($tag_label . '%1$s', '_s') . '</span>', $tags_list); // WPCS: XSS OK.
 			}
 		}
   }
@@ -109,8 +109,8 @@ if ( ! function_exists( '_s_entry_footer' ) ) :
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
 	function _s_entry_footer() {
-    _s_print_categories();
-    _s_print_tags();
+    _s_print_categories("Posted in: ");
+    _s_print_tags("Tagged: ");
 
 		edit_post_link(
 			sprintf(
