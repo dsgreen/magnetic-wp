983d48919bb889f036da6b65e1753dbebbc9fdb8
Doug Green <doug@dsgreen.com>
2018-10-04 12:46:23 -0400

    Comment out emoji removal

--- functions.php
+++ functions.php
@@ -245,5 +245,5 @@ if ( class_exists( 'WooCommerce' ) ) {
  * remove WordPress emoji script from head
  * original source/license: unknown
  */
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
+//remove_action('wp_head', 'print_emoji_detection_script', 7);
+//remove_action('wp_print_styles', 'print_emoji_styles');
