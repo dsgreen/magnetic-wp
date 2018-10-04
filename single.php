0f1c71e5be24e9e0e0d81db07a080ceb24114b1b
Doug Green <doug@dsgreen.com>
2018-10-04 12:49:16 -0400

    Single content markup

--- single.php
+++ single.php
@@ -8,26 +8,30 @@
  */
 
 get_header(); ?>
-<?php get_template_part( 'template-parts/image-header' ); ?>
-  <section class="site-content container" id="content">
-    <main class="site-main" id="main" role="main">
+<section class="site-content container" id="content">
+  <div class="row">
+    <div class="col col-8<?php if (!is_active_sidebar( 'sidebar-1' ) ) { echo " offset-2"; } ?>">
+      <main class="site-main" id="main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+      <?php
+      while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', 'single' );
+        get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+        the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+          comments_template();
+        endif;
 
-		endwhile; // End of the loop.
-		?>
+      endwhile; // End of the loop.
+      ?>
 
-		</main>
-  </section>
+      </main>
+    </div>
+    <?php get_sidebar(); ?>
+  </div>
+</section>
 <?php
 get_footer();
