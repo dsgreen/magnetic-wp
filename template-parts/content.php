ddf8062a429faf133467ad89b770c4b2f98533ca
Doug Green <doug@dsgreen.com>
2018-10-04 12:50:27 -0400

    Content markup

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,54 +10,55 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="post-wrap row">
-	<?php //_s_post_thumbnail('extra_large'); ?>
-
-	<div class="col col-4">
+  <div class="post-wrap">
     <header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+      <div class="entry-meta">
+      <?php
+      _s_print_categories();
+      ?>
+      </div>
+      <?php
+      if ( is_singular() ) :
+        the_title( '<h1 class="entry-title">', '</h1>' );
+      else :
+        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+      endif;
+      ?>
+      <div class="entry-meta">
+      <?php
+      if ( 'post' === get_post_type() ) :
+        _s_posted_on();
+        _s_posted_by();
+        _s_comments_link();
+      endif; ?>
+      </div>
+    </header>
+    <div class="entry-content">
+    <?php _s_post_thumbnail('extra_large'); ?>
+    <?php
+      the_content( sprintf(
+        wp_kses(
+          /* translators: %s: Name of current post. Only visible to screen readers */
+          __( 'Continue reading<span class="sr-only"> "%s"</span>', '_s' ),
+          array(
+            'span' => array(
+              'class' => array(),
+            ),
+          )
+        ),
+        get_the_title()
+      ) );
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-				_s_posted_on();
-				_s_posted_by();
-				_s_comments_link();
-			?>
-		</div>
-		<?php
-		endif; ?>
-	</header>
-  <footer class="entry-footer">
-    <?php _s_entry_footer(); ?>
-  </footer>
-  </div>
-
-	<div class="entry-content col col-8">
-		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="sr-only"> "%s"</span>', '_s' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div>
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+    </div>
+    <footer class="entry-footer">
+      <?php
+      _s_entry_footer();
+      ?>
+    </footer>
   </div>
 </article>
