cf29d61638967859d26957dec47688b738e73d80
Doug Green <doug@dsgreen.com>
2019-04-20 07:39:56 -0400

    Mobile navigation script

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -4,30 +4,69 @@
 
 jQuery(document).ready(function($) {
 
-  // hidden class added for better loading at mobile (expanded menu is not shown as it loads)
+  // hidden class added in HTML for better loading at mobile (expanded menu is not shown as it loads)
+  // remove it after page loads
   $('.main-navigation').removeClass('hidden');
+  $('.mobile-navigation').removeClass('hidden');
 
-  // 1st page load
-  if (window.innerWidth < 768) {
-    $('.main-navigation').hide();
+  /**
+   * set up the mobile menu
+   */
+  function setupMobileMenu() {
+    // hide the menu initially
+    $('.mobile-navigation').hide();
+    $('.mobile-navigation .sub-menu').hide();
+
+    // display sub menu on click
+    $('.mobile-navigation .menu-item-has-children > a').click(function(e) {
+      e.preventDefault();
+
+      // set aria expanded attribute
+      $(this).attr('aria-expanded', ($(this).attr('aria-expanded') === 'true') ? 'false' : 'true');
+
+      $(this).next('.sub-menu').slideToggle('fast');
+    });
   }
 
-  // toggle button
-  $('.nav-toggle').click(function(){
-    $('.main-navigation').slideToggle('fast');
+  /**
+   * main menu toggle button (mobile)
+   */
+  $('.nav-toggle').click(function() {
+    $('.mobile-navigation').slideToggle('fast');
+    // toggled class needed for pages with image header
     $('.site-header').toggleClass('toggled');
   });
 
-  // browser resize, collapse mobile menu
+  /**
+   * 1st page load
+   */
+  setupMobileMenu();
+
+  // mobile
+  // if (window.innerWidth < 768) {}
+
+  /**
+   * browser resize, collapse mobile menu
+   */
+  /*
   $(window).resize(function() {
+    // toggled class needed for pages with image header on mobile
     $('.site-header').removeClass('toggled');
+
+    // desktop
     if (window.innerWidth > 767) {
-      $('.main-navigation').show();
+      // $('.main-navigation').show();
+      // undo the slideToggle stuff, mobile menu
+      // $('.sub-menu').attr('style', '');
     }
+    // mobile
     else if (window.innerWidth < 768) {
-      $('.main-navigation').hide();
+      // hide nav if it's open
+      $('.mobile-navigation').hide();
+      $('.mobile-navigation .sub-menu').hide();
     }
   });
+  */
 
   // add class when scrolling, for pages with transparent header
   $(window).scroll(function() {
