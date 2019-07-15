aa491e006466cfbcc4ffc641e05291e3ada5850c
Doug Green <doug@dsgreen.com>
2019-07-15 10:27:48 -0400

    Minor changes from _s

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="profile" href="https://gmpg.org/xfn/11">
 <?php wp_head(); ?>
 </head>
 <?php if (
