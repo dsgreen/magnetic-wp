fab319cb7f8ab370d53d7eb737c09200be4c7092
Doug Green <doug@dsgreen.com>
2019-07-16 17:28:09 -0400

    Add front page template

---
+++ front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * Front Page template with a background image header, maximum width.
+ *
+ * @package magnetic
+ */
+
+get_header();
+?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+<?php get_template_part( 'template-parts/image-header' ); ?>
+<section class="site-content" id="content">
+  <main class="site-main" id="main">
+
+              <?php get_template_part( 'template-parts/content', 'front-page' ); ?>
+
+          <?php endwhile; // End of the loop. ?>
+
+  </main>
+</section>
+<?php get_footer();
+?>
---
+++ template-parts/content-front-page.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package magnetic
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="container section-intro">
+    <div class="row">
+      <div class="col-12">
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div><!-- .entry-content -->
+      </div>
+    </div>
+  </div>
+  <?php 
+  // Section 1
+  if ( is_active_sidebar( 'home-1-1' ) || is_active_sidebar( 'home-1-2' ) ) : ?>
+  <div class="row no-gutters align-items-center section-1">
+    <div class="col-md-6">
+      <?php if ( is_active_sidebar( 'home-1-1' ) ) : ?>
+        <?php dynamic_sidebar( 'home-1-1' ); ?>
+      <?php endif; ?>
+    </div>
+    <div class="col-md-6">
+      <div class="wrap-inner">
+        <?php if ( is_active_sidebar( 'home-1-2' ) ) : ?>
+          <?php dynamic_sidebar( 'home-1-2' ); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+  <?php endif; ?>
+	<?php 
+  // Section 2
+  if ( is_active_sidebar( 'home-2-1' ) || is_active_sidebar( 'home-2-2' ) ) : ?>
+  <div class="row no-gutters align-items-center section-2">
+    <div class="col-md-6 order-md-last">
+      <?php if ( is_active_sidebar( 'home-2-2' ) ) : ?>
+        <?php dynamic_sidebar( 'home-2-2' ); ?>
+      <?php endif; ?>
+    </div>
+    <div class="col-md-6 order-md-first">
+      <div class="wrap-inner">
+        <?php if ( is_active_sidebar( 'home-2-1' ) ) : ?>
+          <?php dynamic_sidebar( 'home-2-1' ); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+	<?php endif; ?>
+	<?php 
+  // Section 3
+  if ( is_active_sidebar( 'home-3-1' ) || is_active_sidebar( 'home-3-2' ) ) : ?>
+  <div class="row no-gutters align-items-center section-3">
+    <div class="col-md-6">
+      <?php if ( is_active_sidebar( 'home-3-1' ) ) : ?>
+        <?php dynamic_sidebar( 'home-3-1' ); ?>
+      <?php endif; ?>
+    </div>
+    <div class="col-md-6">
+      <div class="wrap-inner">
+        <?php if ( is_active_sidebar( 'home-3-2' ) ) : ?>
+          <?php dynamic_sidebar( 'home-3-2' ); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+	<?php endif; ?>
+	<?php 
+  // Section 4
+  if ( is_active_sidebar( 'home-4-1' ) || is_active_sidebar( 'home-4-2' ) ) : ?>
+  <div class="row no-gutters align-items-center section-4">
+    <div class="col-md-6 order-md-last">
+      <?php if ( is_active_sidebar( 'home-4-2' ) ) : ?>
+        <?php dynamic_sidebar( 'home-4-2' ); ?>
+      <?php endif; ?>
+    </div>
+    <div class="col-md-6 order-md-first">
+      <div class="wrap-inner">
+        <?php if ( is_active_sidebar( 'home-4-1' ) ) : ?>
+          <?php dynamic_sidebar( 'home-4-1' ); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+	<?php endif; ?>
+	<?php
+	// Section 5
+	if ( is_active_sidebar( 'home-5' ) ) : ?>
+  <div class="section-5">
+  <div class="container ">
+    <div class="row no-gutters align-items-center">
+      <div class="col-12">
+        <?php dynamic_sidebar( 'home-5' ); ?>
+      </div>
+    </div>
+  </div>
+  </div>
+	<?php endif; ?>
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="sr-only">%s</span>', 'magnetic' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
