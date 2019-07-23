0b088dcce31acd19af2ecfd6a763fcac3faec4c9
Doug Green <doug@dsgreen.com>
2019-07-23 11:03:20 -0400

    Add nofollow to footer links and theme screenshot

--- footer.php
+++ footer.php
@@ -57,8 +57,8 @@
 	      <?php } else { ?>
           <p>&copy; <?php esc_html_e('Copyright ', 'magnetic'); echo date('Y') . ' '; bloginfo( 'name' );
           esc_html_e('. Proudly powered by ', 'magnetic'); ?>
-          <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'magnetic' ) ); ?>"><?php esc_html_e('WordPress', 'magnetic'); ?></a><?php esc_html_e(' and ', 'magnetic'); ?>
-          <a href="<?php echo esc_url( __( 'https://magneticthemes.com/', 'magnetic') ); ?>"><?php esc_html_e('Magnetic', 'magnetic'); ?></a>.</p>
+          <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'magnetic' ) ); ?>" rel="nofollow"><?php esc_html_e('WordPress', 'magnetic'); ?></a><?php esc_html_e(' and ', 'magnetic'); ?>
+          <a href="<?php echo esc_url( __( 'https://magneticthemes.com/', 'magnetic') ); ?>" rel="nofollow"><?php esc_html_e('Magnetic', 'magnetic'); ?></a>.</p>
 	      <?php } ?>
       </div>
       <div class="col-sm-6">
