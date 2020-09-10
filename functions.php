9e37bac85c7850f10c8c58de7087f58204e86e46
Doug Green <doug@dsgreen.com>
2020-09-10 11:51:48 -0400

    Remove custom header feature

--- functions.php
+++ functions.php
@@ -344,11 +344,6 @@ function magnetic_scripts() {
 add_action( 'wp_enqueue_scripts', 'magnetic_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
