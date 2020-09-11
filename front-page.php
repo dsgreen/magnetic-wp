5cf9b54c7db018072e79b7062a43f52bc8734b48
Doug Green <doug@dsgreen.com>
2020-09-11 16:04:59 -0400

    Main element should not be child of section element, code validation

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@ get_header();
 
 <?php while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'template-parts/image-header' ); ?>
-<section class="site-content" id="content">
+<div class="site-content" id="content">
   <main class="site-main" id="main">
 
               <?php get_template_part( 'template-parts/content', 'front-page' ); ?>
@@ -19,5 +19,5 @@ get_header();
           <?php endwhile; // End of the loop. ?>
 
   </main>
-</section>
+</div>
 <?php get_footer();
