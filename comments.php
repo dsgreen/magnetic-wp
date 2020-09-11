d00889a05a0db38de31e5f5a47b7d126b38a8920
Doug Green <doug@dsgreen.com>
2020-09-11 10:45:37 -0400

    Code sniffer comment, code formatting

--- comments.php
+++ comments.php
@@ -24,7 +24,8 @@ if ( post_password_required() ) {
 
 	<?php
 	// You can start editing here -- including this comment!
-	if ( have_comments() ) : ?>
+	if ( have_comments() ) :
+		?>
 		<h2 class="comments-title">
 			<?php
 			$comment_count = get_comments_number();
@@ -35,10 +36,10 @@ if ( post_password_required() ) {
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			} else {
-				printf( // WPCS: XSS OK.
+				printf(
 					/* translators: 1: comment count number, 2: title. */
 					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'magnetic' ) ),
-					number_format_i18n( $comment_count ),
+					number_format_i18n( $comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			}
@@ -49,20 +50,24 @@ if ( post_password_required() ) {
 
 		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-          'avatar_size' => '72'
-				) );
+				wp_list_comments(
+				  array(
+            'style'      => 'ol',
+            'short_ping' => true,
+            'avatar_size' => '72'
+				  )
+        );
 			?>
 		</ol><!-- .comment-list -->
 
-		<?php the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) : ?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'magnetic' ); ?></p>
 		<?php
+      the_comments_navigation();
+
+      // If comments are closed and there are comments, let's leave a little note, shall we?
+      if ( ! comments_open() ) :
+          ?>
+			  <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'magnetic' ); ?></p>
+    <?php
 		endif;
 
 	endif; // Check for have_comments().
